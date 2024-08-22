45079dad10a86bfd42e31dad4e20754f5974dac2
Chege2004 <chegemanasseh2@gmail.com>
2024-08-22 09:03:17 +0300

    Added the code for the eyesonme and keypda files

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+export default function EyeOnMe() {
+    const onFocus=()=>console.log("Good!");
+    const onBlur=()=>console.log("Hey! Eyes on me!");
+    return(
+        <div>
+            <button onFocus={onFocus} onBlur={onBlur}>Eyes on me</button>
+        </div>
+    ) 
+}
